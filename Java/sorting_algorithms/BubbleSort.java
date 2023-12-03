537633db0ef45f22c85cb7e5288f939c212476bb
Saif Darwish <135634179+saifdarwish@users.noreply.github.com>
2023-12-03 09:41:15 +0100

    Update BubbleSort.java

--- Java/sorting_algorithms/BubbleSort.java
+++ Java/sorting_algorithms/BubbleSort.java
@@ -4,22 +4,22 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] array = {64, 34, 25, 12, 22, 11, 90};
 
-        System.out.println("Unsortiertes Array:");
+        System.out.println("Unsorted Array:");
         printArray(array);
 
         bubbleSort(array);
 
-        System.out.println("\nSortiertes Array:");
+        System.out.println("\nSorted Array:");
         printArray(array);
     }
 
-    // Bubble Sort Algorithmus
+    // Bubble Sort Algorithm
     static void bubbleSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // Tausche arr[j] und arr[j+1]
+                    // Swap arr[j] and arr[j+1]
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
@@ -28,7 +28,7 @@ public class BubbleSort {
         }
     }
 
-    // Hilfsmethode zum Anzeigen eines Arrays
+    // Helper method to display an array
     static void printArray(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n; ++i) {
